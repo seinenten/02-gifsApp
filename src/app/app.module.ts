e34a69cade6b1978caade422898eb7987a64330f
jorge <20200707@uthh.edu.mx>
2023-09-27 23:01:08 -0600

    a
    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 
 import { SharedModule } from './shared/shared.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,12 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     SharedModule,
     GifsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     
   ],
   providers: [],
